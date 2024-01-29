58718c8907eddb09baf9dca6e1b261ba6749887d
JoanChege <jncheg@gmail.com>
2024-01-29 21:22:46 +0300

    feat: add react and route imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Navbar from "./components/NavBar/navbar";
-import Intro from "./components/Intro/intro";
-import About from "./components/About/about";
-import  Project from "./components/Project/project";
-import Contact from "./components/Contact/contact";
-import Footer from "./components/Footer/footer";
+import React from 'react';
+import Navbar from './components/NavBar/navbar';
+import Intro from './components/Intro/intro';
+import About from './components/About/about';
+import Project from './components/Project/project';
+import Contact from './components/Contact/contact';
+import Footer from './components/Footer/footer';
+import { Route } from 'react-router-dom'; 
 
 function App() {
   return (
