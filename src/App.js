2b087d0d14c9c21471159487b5826f04effd6813
JoanChege <jncheg@gmail.com>
2024-01-29 21:25:27 +0300

    feat: remove the routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import Navbar from './components/NavBar/navbar';
-import Intro from './components/Intro/intro';
-import About from './components/About/about';
-import Project from './components/Project/project';
-import Contact from './components/Contact/contact';
-import Footer from './components/Footer/footer';
-import { Route } from 'react-router-dom'; 
+import Navbar from "./components/NavBar/navbar";
+import Intro from "./components/Intro/intro";
+import About from "./components/About/about";
+import  Project from "./components/Project/project";
+import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Route path="/" exact component={Intro} />
-      <Route path="/about" component={About} />
-      <Route path="/project" component={Project} />
-      <Route path="/contact" component={Contact} />
-      <Footer />
+      <Intro/>
+      <About/>
+      <Project/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
