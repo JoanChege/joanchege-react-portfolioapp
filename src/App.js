1d7b9ed0b2950a5ac256629938e0d83b902f7068
JoanChege <jncheg@gmail.com>
2024-01-29 21:26:55 +0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import Navbar from "./components/NavBar/navbar";
-import Intro from "./components/Intro/intro";
-import About from "./components/About/about";
-import  Project from "./components/Project/project";
-import Contact from "./components/Contact/contact";
-import Footer from "./components/Footer/footer";
+import React from 'react';
+import Navbar from './components/NavBar/navbar';
+import Intro from './components/Intro/intro';
+import About from './components/About/about';
+import Project from './components/Project/project';
+import Contact from './components/Contact/contact';
+import Footer from './components/Footer/footer';
+import { Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Intro/>
-      <About/>
-      <Project/>
-      <Contact/>
-      <Footer/>
+      <Route path="/" exact component={Intro} />
+      <Route path="/about" component={About} />
+      <Route path="/project" component={Project} />
+      <Route path="/contact" component={Contact} />
+      <Footer />
     </div>
   );
 }
