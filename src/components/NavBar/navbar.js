849d2c64c2eb3cdafbbbf84a7ccb21ddcaf22e47
JoanChege <jncheg@gmail.com>
2024-01-29 16:35:55 +0300

    feat: add react-scroll, add navbar styles

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './navbar.css';
+import { Link } from 'react-scroll';
 
 
 const Navbar = () => {
   return (
     <div>
-      navbar
+      <nav className='navbar'>
+        <div className='nav-logo'>
+            <p className='nav-name'>Portfolio</p>
+        </div>
+        <div className='desktopMenu'>
+            <Link className='desktopMenuListItem'>Home</Link>
+            <Link className='desktopMenuListItem'>About</Link>
+            <Link className='desktopMenuListItem'>Projects</Link>
+            <Link className='desktopMenuListItem'>Contact</Link>
+        </div>
+      </nav>
     </div>
   )
 }
