2ddf5d401309337e851614797744f1411073171f
JoanChege <jncheg@gmail.com>
2024-01-29 20:27:35 +0300

    feat: add github icon

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './footer.css';
 import ig from '../../assets/insta.png';
 import li from '../../assets/link.png';
+import gi from '../../assets/git.png';
 
 const Footer = () => {
   return (
@@ -12,7 +13,7 @@ const Footer = () => {
             <div className='footer-social-icons'>
                  <img src={ig} alt="Instagram" className='icon'/> 
                  <img src={li} alt="LinkedIn" className='icon'/> 
-                <div class="icon"><i class="uil uil-github-alt"></i></div>
+                 <img src={gi} alt="Github" className='icon'/> 
             </div>
             <div class="bottom-footer">
                 <p>Copyright &copy; Joan Chege  -  2024 All rights reserved</p>
