69ebce47aeb6578cf70f56bb0ed6596f18410c73
JoanChege <jncheg@gmail.com>
2024-01-29 19:52:18 +0300

    feat: add the style

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -4,7 +4,21 @@ import './contact.css';
 const Contact = () => {
   return (
     <div>
-      
+      <section class="section" id="contact">
+                <div class="top-header">
+                  <h1>Get in touch</h1>
+                  <span>If you want a website done, conatct me</span>
+                </div>
+                <div class="row">
+                    <div class="col_1">
+                        <div class="contact-info">
+                            <h2>Find Me <i class="uil uil-corner-right-down"></i></h2>
+                            <p><i class="uil uil-envelope"></i> Email: jncheg@gmail.com</p>
+                            <p><i class="uil uil-phone"></i> Tel: +254712985749</p>
+                        </div>
+                    </div>
+                </div>
+         </section>
     </div>
   )
 }
