1bdb7a363874d4d830e1057a2195b54fc6dbff72
JoanChege <jncheg@gmail.com>
2024-01-29 20:01:50 +0300

    feat: add footer styles

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,8 +3,18 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='footer'>
+        <div class="top-footer">
+                <p>Joan Chege.</p>
+            </div>
+            <div className='footer-social-icons'>
+                <div class="icon"><i class="uil uil-instagram"></i></div>
+                <div class="icon"><i class="uil uil-linkedin-alt"></i></div>
+                <div class="icon"><i class="uil uil-github-alt"></i></div>
+            </div>
+            <div class="bottom-footer">
+                <p>Copyright &copy; Joan Chege  -  2024 All rights reserved</p>
+        </div>
     </div>
   )
 }
