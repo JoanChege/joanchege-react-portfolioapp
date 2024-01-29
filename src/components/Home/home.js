aebda813958787e1bfb15e4620fa3e41b9fe712b
JoanChege <jncheg@gmail.com>
2024-01-29 17:37:57 +0300

    feat: add home section

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -4,8 +4,15 @@ import './home.css';
 const Home = () => {
   return (
     <div>
-      <section id='intro'>
-        
+      <section id='home'>
+        <div className='featured-text'>
+            <span className='hello'>HELLO</span>
+            <span className='introtext'>I am Joan Chege</span>
+            <p className='intropara'>
+            Currently, I am learning front-end development to be a dedicated front-end devoloper who 
+            crafts visually stunning and user-friendly websites. If interested scroll down and get to know more about me.
+            </p>
+          </div>
       </section>
     </div>
   )
