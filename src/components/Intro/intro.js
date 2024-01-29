054b00b5afc7d50e9c0ddebd4770b87d167895fd
JoanChege <jncheg@gmail.com>
2024-01-29 22:17:53 +0300

    feat: add className spanh

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -7,7 +7,9 @@ const Intro = () => {
     <div className='wrapper'>
       <section className='featured-box' id='home'>
         <div className='featured-text'>
+          <div className='spanh'>
             <span className='hello'>HELLO!</span>
+            </div>
             <span className='introtext'>I am Joan Chege</span>
             <p className='intropara'>
             Currently, I am learning front-end development to be a dedicated front-end devoloper who 
