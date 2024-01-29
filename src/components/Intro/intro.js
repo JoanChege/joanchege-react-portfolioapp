082064417b066125502be6ece865b8e2ead3d126
JoanChege <jncheg@gmail.com>
2024-01-29 18:42:13 +0300

    feat: add background color

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
     <div className='wrapper'>
       <section className='featured-box' id='home'>
         <div className='featured-text'>
-            <span className='hello'>HELLO</span>
+            <span className='hello'>HELLO!</span>
             <span className='introtext'>I am Joan Chege</span>
             <p className='intropara'>
             Currently, I am learning front-end development to be a dedicated front-end devoloper who 
