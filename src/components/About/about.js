938679dfab4f284b9d6bbad12f858418e3b18b10
JoanChege <jncheg@gmail.com>
2024-01-29 19:35:32 +0300

    feat: add about css

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -3,12 +3,62 @@ import './about.css'
 
 const About = () => {
   return (
-    <div className='wrapper'>
+    <div>
       <section className='section' id='about' >
         <div className='top-header'>
             <h1>About Me</h1>
         </div>
-        
+        <div className='row'>
+            <div className="col">
+                <div className='about-info'>
+                    <h3>My Introduction</h3>
+                    <p>I am well-versed in HTML, CSS and JavaScript , and other cutting edge
+                             frameworks such as ReactJS and its libraries,which allows me to implement interactive features.                        
+                          </p>
+                </div>
+            </div>
+            <div class="col">
+                      <div class="skills-box">
+                          <div class="skills-header">
+                              <h3>Frontend</h3>
+                          </div>
+                          <div class="skills-list">
+                              <span>HTML</span>
+                              <span>CSS</span>
+                              <span>JavaScript</span>
+                          </div>
+                      </div>
+                      <div class="skills-box">
+                          <div class="skills-header">
+                              <h3>Backend</h3>
+                          </div>
+                          <div class="skills-list">
+                              <span>PHP</span>
+                              <span>JAVA</span>
+                              <span>Python</span>
+                              <span>NodeJS</span>
+                          </div>
+                      </div>
+                      <div class="skills-box">
+                          <div class="skills-header">
+                              <h3>Database</h3>
+                          </div>
+                          <div class="skills-list">
+                              <span>MySQL</span>
+                              <span>MongoDB</span>
+                          </div>
+                      </div>
+                      <div class="skills-box">
+                        <div class="skills-header">
+                            <h3>Frameworks</h3>
+                        </div>
+                        <div class="skills-list">
+                            <span>ReactJS</span>
+                            <span>FLASK</span>
+                        </div>
+                    </div>
+                  </div>
+        </div>
       </section>
     </div>
   )
