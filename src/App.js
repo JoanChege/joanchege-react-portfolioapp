baee8a9d7f3f3c60a4cd8e07747c4ba20873466f
JoanChege <jncheg@gmail.com>
2024-01-29 17:48:49 +0300

    refactor: remove home import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Navbar from "./components/NavBar/navbar";
-import Home from "./components/Home/home";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home/>
     </div>
   );
 }
