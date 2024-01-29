c6e6a53b045adaaedac78a1966e95a7f64ae04eb
JoanChege <jncheg@gmail.com>
2024-01-29 18:00:01 +0300

    feat: add section

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -4,9 +4,18 @@ import './intro.css';
 const Intro = () => {
   return (
     <div>
-      
+      <section id='home'>
+        <div className='featured-text'>
+            <span className='hello'>HELLO</span>
+            <span className='introtext'>I am Joan Chege</span>
+            <p className='intropara'>
+            Currently, I am learning front-end development to be a dedicated front-end devoloper who 
+            crafts visually stunning and user-friendly websites. If interested scroll down and get to know more about me.
+            </p>
+        </div>
+        </section>
     </div>
   )
 }
 
-export default Intro
+export default Intro;
