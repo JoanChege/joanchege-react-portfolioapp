197d2575bfb8f6c9e78f974c8a7acd43763e8778
JoanChege <jncheg@gmail.com>
2024-01-29 18:56:05 +0300

    feat: add styles

---
+++ src/components/About/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './about.css'
+
+const About = () => {
+  return (
+    <div className='wrapper'>
+      <section className='section' id='about' >
+        <div className='top-header'>
+            <h1>About Me</h1>
+        </div>
+        
+      </section>
+    </div>
+  )
+}
+
+export default About;
